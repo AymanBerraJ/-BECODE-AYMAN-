40737adf0cd8a934cbecb5c86d7c631ab9b0f9fa
Ayman Berraj <beayman35@gmail.com>
2024-04-18 09:54:24 +0200

    premier commit

---
+++ app.js
@@ -0,0 +1,71 @@
+/* alert(5.45)
+alert("Viens habibi")
+alert("Non je rigole")
+
+// Bien ou quoi le sang
+/* Bien et toi le sang  
+var x = 50;
+var prenom ="Ayman", nom="Berraj", age=23;
+var homme = true;
+
+alert("Variable de prénom :" + (typeof(prenom)) +
+"\nVariable de nom :" + (typeof(nom)) +
+"\nVariable de âge :" + (typeof(age))+
+"\nVariable de homme :" +(typeof(homme)));
+
+/* 
+x += 2 === x = x + 2
+x -= 2 === x = x - 2
+x /= 2 === x = x / 2
+x *= 2 === x = x * 2
+x %= 2 === x = x % 2 
+
+
+var test = 4 + "1" + 2; // la réponse sera => 412, 4 est un chiffre(int), 1 est un string et 2 est un chiffre(int)
+var test2 = "1" + 4 + 2; // la réponse sera => 142, si on commence avec un string tout le reste devient un string
+var test3 = 4 + 2 + "1"; // la réponse sera => 61, 6 est la réponse de 4+2 et 1 est un string
+alert(test +
+"\n" + test2 +
+"\n" + test3);
+
+var nb = 20;
+
+if (nb <= 18 && nb <= 20){
+    alert("le nombre est bon : " + nb);
+} else{
+    alert("Le nombre n'est pas bon : " + nb);
+}
+
+var mime = 23, hum = "";
+hum = (mime < 22) ? "il a 21 ans" : "Il a plus de 21 ans"; // condition IF abreger++
+alert(hum);
+
+var mom = 12;
+switch (mom) {
+    case 10:
+        alert("il est 10 heure");
+        break;
+    case 23:
+        alert("il est 23 heure");
+        break;
+    case 12:
+        alert("il est 12 heure");
+        break;
+    default:
+        alert("Rien de prévu pour maintenant")
+        break;
+} */
+
+console.log("I am in the console");
+
+let myName = "Henrique";
+console.log(myName);
+alert(myName);
+
+let tauxD = 0.93;
+
+let euro = 50 * tauxD;
+let euro1 = 132 * tauxD;
+
+console.log("Pour avoir 50 dollar, nous avons besoin de " + euro + " et pour 132 dollar nous avons besoinn de " + euro1 );
+
