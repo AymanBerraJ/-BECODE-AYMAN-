f7530cb5f3378aee41624936d51af6e92a9adbf2
Ayman Berraj <beayman35@gmail.com>
2024-04-26 17:18:43 +0200

    algo2 ex 8

--- algo2.js
+++ algo2.js
@@ -36,11 +36,11 @@
 // const multiple = (nb1, longueur) => {
 //     let nb = [];
 //     for(let i = 1; i <= longueur; i++){
-
+//////////////// 1er methode
 //         nb1 = nb1 * i
 //         nb.push(nb1)   // 5 = 5*1 => 5 = 5 * 2 => 10 = 5 * 3 => 30 = 10 * 3
 //         nb1 = nb1 / i
-
+//////////////// 2eme methode
 //         let temp = nb1 * i
 //         nb.push(temp)
 
@@ -52,5 +52,59 @@
 
 5.
 
+// const stringNumber = number => parseFloat(number).toString()
 
+
+// const stringNumber = number => number = number * 1        //JE DETESTE CET EXO
+
+
+// console.log(stringNumber("230.000"));
+// console.log(stringNumber("00402"));
+// console.log(stringNumber("03.1400"));
+// console.log(stringNumber("30"));
+
+6.
+
+// const triage = (array) => array.sort()
+
+// console.log(triage([[6], 5, 2, [3], 1]));
+
+7.
+
+// const calculator = (nombre, operation, nombre2) => {
+
+//     if (operation == "+") {
+//         return nombre + nombre2
+//     }else if(operation == "-"){
+//         return nombre - nombre2
+//     }else if(operation == "*"){
+//         return nombre * nombre2
+//     }else if(operation == "/"){
+//         if(nombre2 == 0){
+//             return "Impossible de diviser par 0"
+//         }
+//         return nombre / nombre2
+//     }
+// }
+// console.log(calculator(2, "+", 2) ); 
+// console.log(calculator(2, "*", 2));
+// console.log(calculator(4, "/", 2));
+
+8.
+
+// const superficie = (pays, region) => {
+    
+//     const superTerre = 148940000;
+//     const calculSuper = (region/superTerre)*100
+//     const modulo = calculSuper.toFixed(2)
+
+//     return `La/Le ${pays} représente ${modulo}% de la superficie de la Terre`
+
+// }
+
+// console.log(superficie("Russia", 17098242));
+// console.log(superficie("USA", 9372610));
+// console.log(superficie("Iran", 1648195));
+
+9. 
 
