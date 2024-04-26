9d6240a0d9f71a02e371107ab14c35acf6f9755e
Ayman Berraj <beayman35@gmail.com>
2024-04-26 11:08:19 +0200

    algo2 ex5

--- algo2.js
+++ algo2.js
@@ -1,8 +1,56 @@
-1.
+0.
 
-const trueFalse = (bool) => bool.reduce((sum, valeurCourante) => (sum += valeurCourante), 0);
+// const trueFalse = (bool) => bool.reduce((sum, valeurCourante) => (sum += valeurCourante), 0);
 
 // true == 1 et false == 0
 
+1.
+
+// const jeu = (a, b) => b - a > 0 && b - a <= 6
+
+
+    // if(b - a > 0 && b - a <= 6){
+    //     return true
+    // }
+    // return false
+
+    // console.log(jeu(3, 7));
+
 2.
+
+// const totalChiffre = (nb) =>  nb.toString().length
+
+// console.log(totalChiffre(1000));
+
+3.
+
+// const convert = (obj) => Object.entries(obj)
+
+// console.log(convert({a:1, b:2}));
+
+4.
+
+// nb1 * i = nb1
+// longueur.length
+
+// const multiple = (nb1, longueur) => {
+//     let nb = [];
+//     for(let i = 1; i <= longueur; i++){
+
+//         nb1 = nb1 * i
+//         nb.push(nb1)   // 5 = 5*1 => 5 = 5 * 2 => 10 = 5 * 3 => 30 = 10 * 3
+//         nb1 = nb1 / i
+
+//         let temp = nb1 * i
+//         nb.push(temp)
+
+//     }
+//     return nb
+// }
+
+// console.log(multiple(7, 5));
+
+5.
+
+
 
