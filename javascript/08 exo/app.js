46b666fddfb3625085641e91fbf878861a10229d
AymanBerraJ <166770154+AymanBerraJ@users.noreply.github.com>
2024-05-29 13:33:57 +0200

    Fin chap8

---
+++ javascript/08 exo/app.js
@@ -0,0 +1,41 @@
+    1.
+
+// const boutonHello = document.querySelector("button");
+
+
+// boutonHello.addEventListener("click", () => {
+//     alert("Oh my God ! You again !!!")
+// })
+
+// boutonHello.nextElementSibling.addEventListener("click", () => {
+//     alert("Thank God ! I thought you'd' never leave !")
+// })
+
+    2.
+
+    // var buttons = document.querySelectorAll("button")
+
+    // buttons.forEach(button => {
+    //     button.addEventListener('click', () => {
+    //         var paragraph = button.nextElementSibling;
+    //         if (paragraph.classList.contains('hidden')){
+    //             paragraph.classList.remove('hidden');
+    //             button.textContent = 'Close tab';
+    //         } else {
+    //             paragraph.classList.add('hidden');
+    //             button.textContent = 'Open tab';
+    //         }
+
+    //     });
+    // });
+
+    3.
+
+    // const button = document.querySelector("button");
+    // const titre = document.querySelector("h1");
+
+    // button.addEventListener('click', () => {
+    //     const random = button.parentElement.style.background = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
+    //     titre.textContent = random
+    // });
+
