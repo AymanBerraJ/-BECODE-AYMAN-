d21d3ca0955fc6048c63e4866a7a7a433e95f8c0
AymanBerraJ <166770154+AymanBerraJ@users.noreply.github.com>
2024-05-29 09:16:06 +0200

    fin exo6b

--- javascript/06.b exo/app.js
+++ javascript/06.b exo/app.js
@@ -1,15 +1,15 @@
-// const bold = document.querySelector("b");
-
-// bold.parentElement.parentElement.parentElement.parentElement.style.backgroundColor = "beige";
-// bold.style.color = "red";
-
-// bold.parentElement.parentElement.parentElement.style.background = "darkblue";
-// bold.parentElement.parentElement.style.color = "white";
-// bold.parentElement.nextElementSibling.style.color = "pink";
-// bold.parentElement.parentElement.parentElement.previousElementSibling.querySelector("ul").style.color = "orange";
-// bold.parentElement.parentElement.parentElement.previousElementSibling.querySelectorAll("a").forEach(a => a.style.color = "green");
-// bold.parentElement.parentElement.parentElement.previousElementSibling.querySelector("h1").style.textTransform = "uppercase";
-
+    1.
+    // const bold = document.querySelector("b");
+    // bold.parentElement.parentElement.parentElement.parentElement.style.backgroundColor = "beige";
+    // bold.style.color = "red";
+    // bold.parentElement.parentElement.parentElement.style.background = "darkblue";
+    // bold.parentElement.parentElement.style.color = "white";
+    // bold.parentElement.nextElementSibling.style.color = "pink";
+    // bold.parentElement.parentElement.parentElement.previousElementSibling.querySelector("ul").style.color = "orange";
+    // bold.parentElement.parentElement.parentElement.previousElementSibling.querySelectorAll("a").forEach(a => a.style.color = "green");
+    // bold.parentElement.parentElement.parentElement.previousElementSibling.querySelector("h1").style.textTransform = "uppercase";
+    
+2.
     // setTimeout(() => {
 
     //     for(let i= 1; i <= 100; i++){
@@ -22,7 +22,7 @@
     //     inner.innerText = "There are a lot of buttons here !"
 
     // }, 2000);
-
+    3.
     // const exemple = document.querySelectorAll("li");
     // exemple.forEach(element => {
         // if(element.classList.contains("highlight")){
@@ -34,10 +34,15 @@
         // toggle
         // element.classList.toggle("highlight");
     // })
+    4.
+    // for(let i = 0; i <= 1025; i++){
+    //     var image = document.createElement("img");
+    //     image.src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+ i + ".png";
+    //     image.alt = "Pokémon " +i;
+    //     document.querySelector(".container").append(image)
+    // }
 
-    for(let i = 0; i <= 1025; i++){
-        var image = document.createElement("img");
-        image.src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+ i + ".png";
-        image.alt = "Pokémon " +i;
-        document.querySelector(".container").append(image)
-    }
+    // const titre = document.querySelector("h1");
+    // titre.style.textAlign = "center";
+    // titre.style.fontSize = "6rem";
+
