88d59665ebef33ec52a44a21e54e581881cc3f07
AymanBerraJ <166770154+AymanBerraJ@users.noreply.github.com>
2024-06-05 18:56:54 +0200

    commit

--- LocaleStorage/modal and slide/modal/app.js
+++ LocaleStorage/modal and slide/modal/app.js
@@ -1,3 +1,17 @@
-const btn = document.createElement("button");
+const btn = document.querySelector("button");
+const text = document.querySelector("#textContent")
+const bcg = document.querySelector(".container");
 
-btn.textContent = "button"
+btn.addEventListener('click', () => {
+    text.style.display = "block";
+    btn.style.display = "none";
+    bcg.style.background= "darkgray";
+    bcg.style.cursor = "pointer";
+})
+
+bcg.addEventListener('click', ()=>{
+    text.style.display = "none";
+    btn.style.display = "block";
+    bcg.style.background = "white";
+});
+const p = document.querySelector("p");
