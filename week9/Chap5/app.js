87cfdb58531cb8eb371ca02f325111ccd8a8edfb
Ayman <beayman35@gmail.com>
2024-06-10 17:10:43 +0200

    chap5 fini

---
+++ week9/Chap5/app.js
@@ -0,0 +1,16 @@
+function bcgColor(color) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        document.body.style.backgroundColor = color;
+        resolve();
+      }, 1000);
+    });
+  }
+
+  bcgColor('blue')
+    .then(() => bcgColor('yellow'))
+    .then(() => bcgColor('red'))
+    .then(() => bcgColor('cyan'))
+    .then(() => bcgColor('violet'))
+    .then(() => bcgColor('green'))
+    .then(() => bcgColor('purple'));
