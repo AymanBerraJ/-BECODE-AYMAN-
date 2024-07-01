02c6d0f09ddfc9986783742dbd5315cd4692aca3
Ayman <beayman35@gmail.com>
2024-07-02 01:17:27 +0200

    nodeJs 03

---
+++ NodeJs/main.js
@@ -0,0 +1,22 @@
+// const part = require('./part');
+// console.log(part.array);
+
+// console.log(part.MathPi);
+// console.log(part.myObj);
+
+// const {MathPi, myObj} = require('./part');
+// console.log(MathPi);
+// console.log(myObj);
+
+// var os = require('os');
+// console.log(os.homedir()); 
+
+const fs = require('fs');
+if(fs.existsSync('./test')&& fs.existsSync('./test/note.txt')){
+    fs.unlinkSync('./test/note.txt');
+    fs.rmdirSync('./test');
+}
+fs.mkdirSync('./test');
+fs.appendFileSync('./test/note.txt', 'I am a BeCode warrior');
+// const data = fs.readFileSync('./test/note.txt', 'utf8');
+// console.log(data);
---
+++ NodeJs/part.js
@@ -0,0 +1,13 @@
+// const array = ["bonbon", "haribo", "gelatine"];
+// module.exports = {array};
+
+
+// const MathPi = 3.14;
+
+// const myObj = {
+//     name : 'Ayman',
+//     age : 24,
+//     city : 'mok'
+// }
+
+// module.exports = {MathPi, myObj}
