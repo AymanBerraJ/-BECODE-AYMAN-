d50a3bc337e935ab69791f516b11ab56555035e7
Ayman <beayman35@gmail.com>
2024-07-02 01:17:57 +0200

    NodeJs 3

--- NodeJs/main.js
+++ NodeJs/main.js
@@ -11,12 +11,12 @@
 // var os = require('os');
 // console.log(os.homedir()); 
 
-const fs = require('fs');
-if(fs.existsSync('./test')&& fs.existsSync('./test/note.txt')){
-    fs.unlinkSync('./test/note.txt');
-    fs.rmdirSync('./test');
-}
-fs.mkdirSync('./test');
-fs.appendFileSync('./test/note.txt', 'I am a BeCode warrior');
+// const fs = require('fs');
+// if(fs.existsSync('./test')&& fs.existsSync('./test/note.txt')){
+//     fs.unlinkSync('./test/note.txt');
+//     fs.rmdirSync('./test');
+// }
+// fs.mkdirSync('./test');
+// fs.appendFileSync('./test/note.txt', 'I am a BeCode warrior');
 // const data = fs.readFileSync('./test/note.txt', 'utf8');
 // console.log(data);
