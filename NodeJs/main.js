b43f5521d7e454a29d8dd73d713154561d7b185b
AymanBerraJ <166770154+AymanBerraJ@users.noreply.github.com>
2024-07-03 09:08:53 +0200

    node 3

--- NodeJs/main.js
+++ NodeJs/main.js
@@ -1,4 +1,7 @@
-// const part = require('./part');
+var os = require('os');
+const fs = require('fs');
+const part = require('./part');
+
 // console.log(part.array);
 
 // console.log(part.MathPi);
@@ -8,11 +11,9 @@
 // console.log(MathPi);
 // console.log(myObj);
 
-// var os = require('os');
 // console.log(os.homedir()); 
 
-// const fs = require('fs');
-// if(fs.existsSync('./test')&& fs.existsSync('./test/note.txt')){
+// if(fs.existsSync('./test') && fs.existsSync('./test/note.txt')){
 //     fs.unlinkSync('./test/note.txt');
 //     fs.rmdirSync('./test');
 // }
@@ -20,3 +21,21 @@
 // fs.appendFileSync('./test/note.txt', 'I am a BeCode warrior');
 // const data = fs.readFileSync('./test/note.txt', 'utf8');
 // console.log(data);
+
+// const create = fs.createReadStream('./hugeText.html');
+// const writeStream = fs.createWriteStream('./newText.html');
+
+// create.pipe(writeStream); // version rapide du createWriteStream
+
+// create.on('data', (chunk) => {
+//     console.log('--------- NEW CHUNK --------');
+//     console.log(chunk);
+
+//     writeStream.write(chunk)
+// })
+
+const http = require('http');
+
+http.createServer(function(req, res){
+    console.log('Listening for requests on port 3000');
+}).listen(3000);
