d654aa328bb13cc0fbc400959da44524f8f0f856
Ayman <beayman35@gmail.com>
2024-07-11 02:45:38 +0200

    oke

--- NodeJs/chap9/server1.js
+++ NodeJs/chap9/server1.js
@@ -3,21 +3,32 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 const path = require("path");
-const httpo = require("http");
+const http = require("http");
 const { log } = require("console");
 const root = __dirname;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.sendFile("./html/main.html", { root });
-  res.status(200);
+  res.status(200).sendFile("./html/main.html", { root });
 });
 
 app.post("/", (req, res) => {
-  const body = req.body;
-  console.log("Received POST body :", body);
-  res.end();
+  const method = req.method;
+  const url = req.url;
+
+  // Save the original send method
+  const originalSend = res.send;
+
+  res.send = function (...args) {
+    const status = res.statusCode;
+    console.log(`Method: ${method}, URL: ${url}, StatusCode: ${status}`);
+
+    // Call the original send function
+    return originalSend.apply(res, args);
+  };
+
+  res.send("POST request received");
 });
 
 app.listen(3000, () => {
--- NodeJs/chap9/server2.js
+++
@@ -1,9 +0,0 @@
-const bodyParser = require("body-parser");
-const express = require("express");
-const app = express();
-const fs = require("fs");
-const path = require("path");
-const httpo = require("http");
-const root = __dirname;
-
-
