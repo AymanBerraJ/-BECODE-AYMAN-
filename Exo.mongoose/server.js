42e900633851b473b3a7e1ab3251abf9226b4d5d
Ayman <beayman35@gmail.com>
2024-07-21 03:50:57 +0200

    exo

--- Exo.mongoose/server.js
+++ Exo.mongoose/server.js
@@ -3,12 +3,22 @@ const app = express();
 const fs = require("fs");
 const http = require("http");
 const path = require("path");
+const { connectToDb, getDb } = require("./db");
 const root = __dirname;
 const port = 3000;
 
 const users = [];
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+
+connectToDb((err) => {
+    if (!err) {
+      app.listen(port, () => {
+        console.log(`app listening on PORT ${port}`);
+      });
+      db = getDb();
+    }
+  });
 
 
 app.get('/', (req, res)=>{
@@ -22,16 +32,39 @@ app.post('/', (req, res)=>{
     if (name) {
         users.push(name);
     }
-    res.redirect('/');
+    const task = {name};
+
+//   db.collection("task")
+//     .insertMany(task)
+//     .then((result) => {
+//       res.status(201).json(result);
+//     })
+//     .catch((err) => {
+//       res.status(500).json({ err: "Could not create a new document" });
+//     });
+//     res.redirect('/');
 })
 
 app.delete('/', (req, res)=>{
 
-})
+//     const { id } = req.params;
+//     const { name } = req.body;
+
+//     if (name) {
+//         users = users.filter(user => user !== name);
+//     }
 
 
-
-
-app.listen(port, ()=>{
-    console.log(`Listening to port ${port}`);
+//     if (ObjectId.isValid(id)) {
+//     db.collection("task")
+//       .deleteOne({ _id: new ObjectId(id) })
+//       .then((result) => {
+//         res.status(200).json(result);
+//       })
+//       .catch((err) => {
+//         res.status(500).json({ err: "Could not delete the document" });
+//       });
+//   } else {
+//     res.status(500).json({ err: "Not a valid doc ID" });
+//   }
 })
