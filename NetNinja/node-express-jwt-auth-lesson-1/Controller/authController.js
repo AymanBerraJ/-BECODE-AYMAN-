25a450126cbd9bff1a66c1b11755d9bdb1186840
AymanBerraJ <166770154+AymanBerraJ@users.noreply.github.com>
2024-07-25 18:29:28 +0200

    video 14 part2

--- NetNinja/node-express-jwt-auth-lesson-1/Controller/authController.js
+++ NetNinja/node-express-jwt-auth-lesson-1/Controller/authController.js
@@ -1,4 +1,33 @@
 const User = require("../models/User");
+const jwt = require("jsonwebtoken")
+
+const handleErrors = (err) => {
+  console.log(err.message, err.code);
+  let errors = { email: '', password: ''};
+
+  // validation Errors
+  if(err.message.includes('user validation failed')) {
+    Object.values(err.errors).forEach(({properties}) => {
+      errors[properties.path] = properties.message;
+    })
+  }
+
+  // duplicate error code
+
+  if(err.code === 11000){
+    errors.email = 'that email is already registered';
+    return errors;
+  }
+
+  return errors;
+};
+
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+  return jwt.sign({id}, 'bdma secret', {
+    expiresIn: maxAge
+  });
+}
 
 module.exports.signup_get = (req, res) => {
   res.render("signup");
@@ -13,10 +42,12 @@ module.exports.signup_post = async (req, res) => {
 
   try {
     const user = await User.create({ email, password });
-    res.status(201).json(user);
+    const token = createToken(user._id);
+    res.cookie('jwt', token, {httpOnly: true, maxAge: maxAge * 1000})
+    res.status(201).json({user: user._id});
   } catch (err) {
-    console.log(err);
-    res.status(400).send("User not created");
+    const errors = handleErrors(err);
+    res.status(400).json({errors});
   }
 };
 
@@ -25,6 +56,9 @@ module.exports.login_post = async (req, res) => {
 
   try {
 
-    
-  } catch (error) {}
+    const user = await User.login(email, password);
+    res.status(200).json({ user: user._id});
+  } catch (err) {
+    res.status(400).json({});
+  }
 };
