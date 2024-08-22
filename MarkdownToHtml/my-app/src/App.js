63f0582958a82cc6044153ff99f63d48586815d2
Ayman Berraj <beayman35@gmail.com>
2024-08-22 13:05:32 +0200

    finito markdown

--- MarkdownToHtml/my-app/src/App.js
+++ MarkdownToHtml/my-app/src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState} from "react";
+import marked from "marked";
 
 function App() {
+
+  const [text, setText] = useState('');
+
+  function inputChange (event) {
+    setText(event.target.value)
+  }
+
+  function createMark () {
+    return {__html: marked(text)}
+  }
+
+  const  headingOne = () => {
+    setText(`# ${text} `)
+  }
+  const  headingTwo = () => {
+    setText(`## ${text} `)
+  }
+  const bold = () => {
+    setText(`**${text}**`)
+  }
+  const italic = () => {
+    setText(`*${text}*`)
+  }
+  const line = () => {
+    setText(`${text}\n---\n`)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="App">
+    <header>
+    <h1>Markdown to HTML Converter</h1>
+    </header>
+    <div>
+      <textarea 
+      value={text}
+      onChange={inputChange}
+      placeholder="Markdown..."
+      />
+      <div>
+        <button onClick={headingOne}>Heading 1</button>
+        <button onClick={headingTwo}>Heading 2</button>
+        <button onClick={bold}>Bold</button>
+        <button onClick={italic}>Italic</button>
+        <button onClick={line}>Horizontal Line</button>
+      </div>
+      <div dangerouslySetInnerHTML={createMark()}></div>
     </div>
+  </div>
   );
 }
 
